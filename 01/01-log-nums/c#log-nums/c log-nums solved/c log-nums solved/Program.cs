0db26b73f8c9c30c0cf2677a1788907952bfcc4b
LilTzar <eeoerkeedu@gmail.com>
2022-11-09 11:41:33 -0700

    adjusted files, created log nums c# files

---
+++ 01/01-log-nums/c#log-nums/c log-nums solved/c log-nums solved/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_log_nums_solved
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
