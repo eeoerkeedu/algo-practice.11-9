199ef943614a8b0b72bea45165d74761358bcc06
LilTzar <eeoerkeedu@gmail.com>
2022-11-09 11:30:43 -0700

    initial commit with solution for arithmatic in c#

---
+++ 01/arithmeticAlgo/arithmeticAlgo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arithmeticAlgo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(addition(10, 24));
+            Console.WriteLine(subtract(100, 91));
+            Console.WriteLine(multiply(7, 70));
+            Console.WriteLine(divide(33, 11));
+
+
+            Console.ReadKey();
+
+        }
+        static int addition(int num1, int num2) { return num1 + num2; }
+        static int subtract(int num1, int num2) { return(num1 - num2);  }
+        static int multiply(int num1, int num2) { return (num1 * num2);  }
+        static float divide(int num1, int num2) { return (num1 / num2); }
+    }
+}
